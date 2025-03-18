9135bac0a536a64c387c9bccd46a0eee5e94101a
ShaneMughkerhegl <168695895+ShaneMughkerhegl@users.noreply.github.com>
2025-03-18 17:23:30 +1000

    Add files via upload
    Develop user interactive program for managing guitars

---
+++ prac_06/guitars.py
@@ -0,0 +1,20 @@
+def main(): # Allows users to enter guitars and display them.
+    guitars = []
+    print("My guitars!")
+    while True:
+        name = input("Name: ")
+        if name == "":
+            break
+        year = int(input("Year: "))
+        cost = float(input("Cost: "))
+        guitars.append(Guitar(name, year, cost))
+        print(f"{name} ({year}) : ${cost:,.2f} added.")
+
+    print("\nThese are my guitars:")
+    for i, guitar in enumerate(guitars, 1):
+        vintage_string = " (vintage)" if guitar.is_vintage() else ""
+        print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
+
+
+if __name__ == "__main__":
+    main()
