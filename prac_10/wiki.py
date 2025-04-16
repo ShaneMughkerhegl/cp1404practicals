f874e68aa582d133651760fbceef90347690cafd
ShaneMughkerhegl <168695895+ShaneMughkerhegl@users.noreply.github.com>
2025-04-16 10:28:31 +1000

    Update wiki.py

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -1,11 +1,9 @@
 import wikipedia
 
 def main():
-    """Get user input and display Wikipedia page details until blank input."""
-    while True:
-        title = input("Enter page title: ")
-        if not title:
-            break
+# Get user input and display Wikipedia page details until blank input.
+    title = input("Enter page title: ")
+    while title:
         try:
             page = wikipedia.page(title, auto_suggest=False)
             print(page.title)
@@ -15,7 +13,8 @@ def main():
             print("We need a more specific title. Try one of the following, or a new search:")
             print(e.options)
         except wikipedia.exceptions.PageError:
-            print("Page id "{}" does not match any pages. Try another id!".format(title))
+            print(f'Page id "{title}" does not match any pages. Try another id!')
+        title = input("Enter page title: ")
     print("Thank you.")
 
 if __name__ == "__main__":
